16b5c27214ac1942503fa0530c96b8472f7d6f0e
TAMILMANIsivaraj <89570358+TAMILMANIsivaraj@users.noreply.github.com>
2025-12-14 17:29:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -357,10 +357,10 @@ output "lb_dns_name" {
 
 
 resource "aws_s3_bucket" "example" {
-  bucket = "reyaz-devopsproject-bkt-123"
+  bucket = "tamil-devopsproject-bkt-123"
 
   tags = {
-    Name        = "reyaz-devopsproject-bkt-123"
+    Name        = "tamil-devopsproject-bkt-123"
     Environment = "Dev"
   }
 }
